7c98c598314a288e9cbc9a4cd8199e7d0cff1c9e
krnd <krnd@users.noreply.github.com>
2022-06-12 10:48:26 +0200

    feat: export public functions

--- PSSyncle/PSSyncle.psm1
+++ PSSyncle/PSSyncle.psm1
@@ -1,9 +1,29 @@
 #Requires -Version 5.1
 
 
-Get-ChildItem (Join-Path (Split-Path $script:MyInvocation.MyCommand.Path) "Private") -Filter "*.ps1" -Recurse | ForEach-Object {
+# ################################ PRIVATE #################################
+
+Get-ChildItem `
+    -Path (Join-Path $PSScriptRoot "Private") `
+    -Filter "*.ps1" `
+    -Recurse `
+| ForEach-Object {
+
     . $_.FullName
+
 }
-Get-ChildItem (Join-Path (Split-Path $script:MyInvocation.MyCommand.Path) "Public") -Filter "*.ps1" -Recurse | ForEach-Object {
+
+
+# ################################ PUBLIC #################################
+
+Get-ChildItem `
+    -Path (Join-Path $PSScriptRoot "Public") `
+    -Filter "*.ps1" `
+    -Recurse `
+| ForEach-Object {
+
     . $_.FullName
+
+    Export-ModuleMember `
+        -Function $_.BaseName
 }
