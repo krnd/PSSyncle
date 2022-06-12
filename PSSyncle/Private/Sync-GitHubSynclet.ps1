66e9eff497d6d742ef0de3582fd7ac9420f01296
krnd <krnd@users.noreply.github.com>
2022-06-12 10:49:17 +0200

    feat: rework github sync

--- PSSyncle/Private/Sync-GitHubSynclet.ps1
+++ PSSyncle/Private/Sync-GitHubSynclet.ps1
@@ -6,41 +6,108 @@ function Sync-GitHubSynclet {
     [CmdletBinding(PositionalBinding = $false)]
     param (
         [Parameter(Position = 0, Mandatory = $true)]
+        [ValidateScript({
+            if (-not $_.Repo) {
+                throw "Error 'Synclet[GitHub].Repo': $($Synclet.Repo) ($($Synclet.Target))"
+            } elseif (-not $_.Path) {
+                throw "Error 'Synclet[GitHub].Path': $($Synclet.Path) ($($Synclet.Target))"
+            }
+            return $true
+        })]
         [PSCustomObject]
-        $Synclet
+        $Synclet,
+        [Parameter(Mandatory = $true)]
+        [PSCustomObject]
+        $Config
     )
 
-    if (-not $Synclet.Repo -or $Synclet.Repo -notlike "*?/?*")
-    { throw "Error 'Synclet[GitHub].Repo': $($Synclet.Repo) ($($Synclet.Target))" }
-    if (-not $Synclet.File)
-    { throw "Error 'Synclet[GitHub].File': $($Synclet.File) ($($Synclet.Target))" }
+    $ItemPath = $Synclet.Path
+    if ($ItemPath.EndsWith("/*") -or $ItemPath.EndsWith("\*")) {
+        $ItemPath = $ItemPath.TrimEnd("/\*")
+        $FolderContentOnly = $true
+    } else {
+        $FolderContentOnly = $false
+    }
 
     $OwnerName, $RepositoryName = $Synclet.Repo -split "/"
-    $RemoteFile = Get-GitHubContent `
+    $RemoteItem = Get-GitHubContent `
         -OwnerName $OwnerName `
         -RepositoryName $RepositoryName `
-        -Path $Synclet.File
+        -Path $ItemPath
 
-    if ($Synclet.Target.EndsWith("\") `
-            -or $Synclet.Target.EndsWith("/") `
-            -or (Test-Path $Synclet.Target -PathType Container)) {
-        $TargetPath = $Synclet.Target
-        $TargetFile = (Join-Path $Synclet.Target $RemoteFile.name)
+    $RequestList = @()
+    if ($RemoteItem.Type -eq "dir") {
+        $BasePath = $RemoteItem.Path
+        $DirectoryQueue = [System.Collections.Queue]@($RemoteItem)
+        while ($DirectoryQueue.Count) {
+            $DirectoryQueue.Dequeue().Entries | ForEach-Object {
+                if ($_.Type -eq "dir") {
+                    $DirEntry = Get-GitHubContent `
+                        -OwnerName $OwnerName `
+                        -RepositoryName $RepositoryName `
+                        -Path $_.Path
+                    $DirectoryQueue.Enqueue($DirEntry)
+                } else {
+                    $RequestList += $_
+                }
+            }
+        }
     } else {
-        $TargetPath = (Split-Path $Synclet.Target)
-        $TargetFile = $Synclet.Target
+        $BasePath = $null
+        $RequestList += $RemoteItem
     }
 
-    if ($TargetPath) {
-        (New-Item `
-            -Path $TargetPath `
-            -ItemType Directory `
-            -Force
-        ) | Out-Null
+    $Target = $Synclet.Target
+    if (
+        $Target.EndsWith("/") `
+        -or $Target.EndsWith("\") `
+        -or (Test-Path $Target -PathType Container)
+    ) {
+        $IsTargetFolder = $true
+    } elseif (
+        $Target.EndsWith(".") `
+            -or ($Target -like "*?.?*")
+    ) {
+        $IsTargetFolder = $false
+        $Target = $Target.TrimEnd(".")
+    } else {
+        $IsTargetFolder = $true
     }
-    Invoke-WebRequest `
-        -Uri $RemoteFile.download_url `
-        -OutFile $TargetFile
 
-    return $TargetFile
+    if ($IsTargetFolder -and -not $FolderContentOnly) {
+        $Target = (Join-Path $Target $RemoteItem.Name)
+    }
+
+    $FileList = @()
+    $RequestList | ForEach-Object {
+        $SubPath = (Split-Path $_.Path)
+        if ($SubPath -and $BasePath) {
+            $SubPath = $SubPath.Substring($BasePath.Length).TrimEnd("/")
+        }
+
+        $OutFile = $Target
+        if ($IsTargetFolder) {
+            if ($SubPath) {
+                $OutFile = (Join-Path $OutFile $SubPath)
+            }
+            $OutFile = (Join-Path $OutFile $_.Name)
+        }
+
+        $OutFilePath = (Split-Path $OutFile)
+        if ($OutFilePath) {
+            New-Item `
+                -Path $OutFilePath `
+                -ItemType Directory `
+                -ErrorAction SilentlyContinue `
+                -Force | Out-Null
+        }
+
+        Invoke-WebRequest `
+            -Uri $_.Download_URL `
+            -OutFile $OutFile `
+            -UseBasicParsing
+        $FileList += (Get-Item $OutFile)
+    }
+
+    return $FileList
 }
